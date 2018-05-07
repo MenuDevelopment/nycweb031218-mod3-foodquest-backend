c4ab3eff93683f582cead298285ed34e64ac6851
Manuel Grullon <astermanuelg@gmail.com>
2018-05-07 15:37:09 -0400

    crud setup 💩

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,7 +1,26 @@
 class Api::V1::RestaurantsController < ApplicationController
+  before_action :grab_user
+
   def index
-    @user = User.find(params[:user_id])
     @restaurants = Restaurant.where(user_id: @user.id)
     render json: @restaurants
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def grab_user
+    @user = User.find(params[:user_id])
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,24 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :grab_user, only: [:create, :update, :destroy]
+
   def index
     @users = User.all
     render json: @users
   end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def grab_user
+    @user = User.find(params[:id])
+  end
+
 end
