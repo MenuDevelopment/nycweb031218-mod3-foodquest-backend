fea8fc9da1d3d4a44631d6d98fd7d1c49998ee6d
github username <github email address>
2018-05-07 17:19:46 -0400

    crud for restaurant finished 🍕

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::RestaurantsController < ApplicationController
   before_action :grab_user
+  before_action :grab_restaurant, only: [:update, :destroy]
 
   def index
     @restaurants = Restaurant.where(user_id: @user.id)
@@ -7,21 +8,37 @@ class Api::V1::RestaurantsController < ApplicationController
   end
 
   def create
-
+    @restaurant = Restaurant.create(restaurant_params)
+    if @restaurant.save
+      render json: @restaurant, status: :accepted
+    else
+      render json: { errors: @restaurant.errors.full_messages}, status: :unprocessible_entity
+    end
   end
 
   def update
+    @restaurant.update(restaurant_params)
+    if @restaurant.save
+      render json: @restaurant, status: :accepted
+    else
+      render json: { errors: @restaurant.errors.full_messages}, status: :unprocessible_entity
+    end
 
   end
 
   def destroy
-
+    @restaurant.destroy
+    render json: {body: nil, status: :no_content}
   end
 
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :json, :user_id)
+  end
+
+  def grab_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def grab_user
