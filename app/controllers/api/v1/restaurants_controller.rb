f016506d148da6a38df11abe03190deabf87126e
Manuel Grullon <astermanuelg@gmail.com>
2018-05-07 15:27:25 -0400

    added and tested index to controllers

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::RestaurantsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @restaurants = Restaurant.where(user_id: @user.id)
+    render json: @restaurants
+  end
 end
